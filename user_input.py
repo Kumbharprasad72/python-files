9bac31aca45b760073d7d3a02a8b6aee6760574a
Kumbharprasad72 <142747547+Kumbharprasad72@users.noreply.github.com>
2023-12-11 15:08:16 +0530

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,57 @@
+'''
+code editor--print()---console(output window
+
+if there is need to bring user input from console to the editor or to store in a variable we need
+input() function.
+
+syntax:
+
+      variable=input()
+'''
+
+
+'''
+print("Enter a numbber:")
+x=input()
+#print("Value of x:",x)
+
+print("Enter a Second number:")
+y=input()
+
+z=x+y
+
+print("Addition of",x,"and",y,"is:",z)
+
+
+Input given by user in python is always stored as string by default
+
+if there is need to convert string to number use following functions.
+
+int() : this funcion is used to convert string to integer.
+float(): this function is used to convert string to float.
+
+'''
+'''
+print("Enter a number:")
+x=input()
+print(type(x))
+#a=int(x)
+#print(type(a))
+a=float(x)
+print(type(a))
+'''
+print("Enter a numbber:")
+x=input()
+a=int(x)
+#print("Value of x:",x)
+
+print("Enter a Second number:")
+y=input()
+b=float(y)
+z=a+b
+
+print("Addition of",x,"and",y,"is:",z)
+
+
+
+
